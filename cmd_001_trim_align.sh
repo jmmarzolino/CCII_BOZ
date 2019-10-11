2de44a47bfcc45aea2d7faf748aace54fee82345
Jill Marzolino <jmarz001@ucr.edu>
2019-10-11 12:21:45 -0700

    write correct directories, file names, and trimming command

--- cmd_001_trim_align.sh
+++ cmd_001_trim_align.sh
@@ -23,41 +23,30 @@ INDEX=/rhome/dkoenig/shared/GENOMES/NEW_BARLEY/GENOME_SPLIT/barley_split_referen
 # set directories
 PROJECT_DIR=/rhome/jmarz001/bigdata/CCII_BOZ
 SEQS=${PROJECT_DIR}/args/raw_fqs
-DATA_DIR=${PROJECT_DIR}/data
 TRIMMED=${PROJECT_DIR}/data/trim_fq
 BAMS=${PROJECT_DIR}/data/bams
 
 # create data/file directory structure
-mkdir $DATA_DIR
+mkdir ${PROJECT_DIR}/data
 mkdir $TRIMMED
 mkdir $BAMS
 
 # get filenames from list
-FILE=$(head -n $SLURM_ARRAY_TASK_ID $SEQLIST | tail -n 1)
-# get basename of file, stripping at "."
-#267_S234_L003_R1_001.fastq ==> 267_S234_L003
-NAME=$(basename "$FILE" | cut -d. -f1 | cut -d_ -f1-3)
-# == 267_L003/4
-SHORT=$(basename "$NAME" | cut -d_ -f1,3)
+FILE_1=$(head -n $SLURM_ARRAY_TASK_ID $SEQS | tail -n 1 | cut -f1)
+FILE_2=$(head -n $SLURM_ARRAY_TASK_ID $SEQS | tail -n 1 | cut -f2)
+sample_name=$(head -n ${SLURM_ARRAY_TASK_ID} $FILE_LIST | tail -n1 | cut -f3 | cut -d_ -f1)
+run_name=$(head -n ${SLURM_ARRAY_TASK_ID} $FILE_LIST | tail -n1 | cut -f3 | cut -d_ -f3)
+# rhome/jmarz001/shared/SEQ_RUNS/10_8_2018/FASTQ/250_S229_L003_R1_001.fastq       rhome/jmarz001/shared/SEQ_RUNS/10_8_2018/FASTQ/250_S229_L003_R2_001.fastq       250_S229_L003_001       ACAGTG  Novaseq
 
 # Quality/Adapter trimming
 java -jar $TRIMMOMATIC PE -threads 10 \
-$WORKINGDIR/"$NAME"_R1_001.fastq $WORKINGDIR/"$NAME"_R2_001.fastq \
-$RESULTSDIR/"$SHORT"_1_trimmed_paired.fq $RESULTSDIR/"$SHORT"_1_unpaired.fq \
-$RESULTSDIR/"$SHORT"_2_trimmed_paired.fq $RESULTSDIR/"$SHORT"_2_unpaired.fq \
+$FILE_1 $FILE_2 \
+$TRIMMED/${sample_name}_${run_name}_1.fq.gz $TRIMMED/${sample_name}_${run_name}_1.un.fq.gz \
+$TRIMMED/${sample_name}_${run_name}_1.fq.gz $TRIMMED/${sample_name}_${run_name}_1.un.fq.gz \
 ILLUMINACLIP:"$ADAPTERDIR"/PE_all.fa:2:30:10 \
 SLIDINGWINDOW:4:20 MINLEN:75
 
-${thisfile_1} ${thisfile_2}
-${wkdir}/OUTPUT/TRIM_FASTQS/${sample_name}${run_name}_1.fq.gz ${wkdir}/OUTPUT/TRIM_FASTQS/${sample_name}${run_name}_1_un.fq.gz
-${wkdir}/OUTPUT/TRIM_FASTQS/${sample_name}${run_name}_2.fq.gz ${wkdir}/OUTPUT/TRIM_FASTQS/${sample_name}${run_name}_2_un.fq.gz
 
-
-# select each set of reads from the file in turn
-thisfile_1=`head -n ${SLURM_ARRAY_TASK_ID} $FILE_LIST | tail -n1 | cut -f1`
-thisfile_2=`head -n ${SLURM_ARRAY_TASK_ID} $FILE_LIST | tail -n1 | cut -f2`
-sample_name=`head -n ${SLURM_ARRAY_TASK_ID} $FILE_LIST | tail -n1 | cut -f3`
-run_name=`head -n ${SLURM_ARRAY_TASK_ID} $FILE_LIST | tail -n1 | cut -f4`
 
 
 minimap2 -t 10 -ax sr ~/shared/GENOMES/NEW_BARLEY/2019_Release_Morex_vers2/Barley.mmi -R "@RG\tID:bar_samp_${SLURM_ARRAY_TASK_ID}\tPL:illumina\tSM:${sample_name}\tLB:${sample_name}" ${wkdir}/OUTPUT/TRIM_FASTQS/${sample_name}${run_name}_1.fq.gz ${wkdir}/OUTPUT/TRIM_FASTQS/${sample_name}${run_name}_2.fq.gz > ${wkdir}/OUTPUT/BAM/${sample_name}${run_name}.sam
