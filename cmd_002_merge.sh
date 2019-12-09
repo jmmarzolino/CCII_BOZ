48dba0262d6701178bba56e4bdb13a18c197a029
Jill Marzolino <jmarz001@ucr.edu>
2019-12-09 11:06:19 -0800

    add sort and index

--- cmd_002_merge.sh
+++ cmd_002_merge.sh
@@ -10,6 +10,7 @@
 
 # load modules
 module load samtools/1.9
+#INDEX=/rhome/jmarz001/shared/GENOMES/BARLEY/2019_Release_Morex_vers2/Barley_Morex_V2_pseudomolecules.fasta
 
 # set directories
 PROJECT_DIR=/rhome/jmarz001/bigdata/CCII_BOZ
@@ -24,4 +25,6 @@ FILE_2=`head -n ${SLURM_ARRAY_TASK_ID} $SEQS | cut -f2  | tail -n1`
 
 # merge files and index
 samtools merge -@10 $MERGED_FILE $FILE_1 $FILE_2
-samtools index -c $MERGED_FILE
+# would need to parse the merge file name
+#samtools view -b -T $INDEX $BAMS/$MERGED_FILE | samtools sort -@ 10 > $BAMS/${MERGED_FILE}_sorted.bam
+samtools index -c ${MERGED_FILE}.bam
