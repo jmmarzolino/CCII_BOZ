c55df04c6b24a23d209c873c96572fd127e62355
Jill Marzolino <jmarz001@ucr.edu>
2019-12-09 17:26:52 -0800

    fix how generation numbers are called so loops are efficient

--- fishers.R
+++ fishers.R
@@ -2,7 +2,7 @@
 
 setwd("/bigdata/koeniglab/jmarz001/CCII_BOZ/results")
 library(readr)
-genome_counts <- read_delim("progeny_counts",
+progeny_counts <- read_delim("progeny_counts",
     "\t", escape_double = FALSE, col_names = FALSE,
     trim_ws = TRUE)
 
@@ -39,25 +39,33 @@ pairwise_comp <- data.frame(late, early, DB_transition, Davis_earlylate, Boz_ear
 all6pops <- c(parents, F18, F27, F28, F50, F58)
 allprogeny <- c(F18, F27, F28, F50, F58)
 
-for (row in 1:nrow(merge)) {
+for (row in 1:nrow(progeny_counts)) {
   # take allele counts from each line in turn, convert into a matrix with generations as rows
-  site_counts <- matrix(as.numeric(merge[row,3:12]), nrow=5,byrow = TRUE)
-  r=12
-  for (i in 1:ncol(parent_comp)) {
+  site_counts <- matrix(as.numeric(progeny_counts[row,5:16]),nrow=6,byrow=T)
+  r=12 #number of filled columns in loaded table
+  for (i in 2:ncol(parent_comp)) {
+    parents <- site_counts[1,1:2]
+    progeny <- site_counts[i,1:2]
+    comparison <- matrix(c(parents,progeny), nrow=2, byrow=T)
     # make significance comparisons between the parental counts and each generation in turn
-    p_value <- fisher.test(site_counts[i,1:2],alternative = "two.sided")[1] # conduct fishers on whichever row (generation) and get ref and allele counts (1:2)
-    merge[row,r] <- c(p_value) #assign the fishers p-value to be added to the end of the current row in order (r)
-    r=r+1 
+    p_value <- fisher.test(comparison,alternative = "two.sided",workspace = 2e8)[1] # conduct fishers on whichever row (generation) and get ref and allele counts (1:2)
+    progeny_counts[row,(r+1)] <- p_value #assign the fishers p-value to be added to the end of the current row in order (r)
+    r=r+1
   }
-  for (i in pairwise_comp){
+  for (i in 1:ncol(pairwise_comp)){
+    a <- site_counts[pairwise_comp[1,i],1:2]
+    b <- site_counts[pairwise_comp[2,i],1:2]
+    comparison <- matrix(c(a,b), nrow=2, byrow=T)
+    p_value <- fisher.test(comparison,alternative = "two.sided",workspace = 2e8)[1]
+    progeny_counts[row,r] <- p_value
+    r=r+1
+  }
+  p_value <- fisher.test(site_counts[allprogeny,1:2],alternative = "two.sided",workspace = 2e8)[1]
+  progeny_counts[row,r] <- p_value
+  r=r+1
 
-  }
-  for (i in all6pops){
-
-  }
-  for (i in allprogeny){
-
-  }
+  p_value <- fisher.test(site_counts[all6pops,1:2],alternative = "two.sided",workspace = 2e8)[1]
+  progeny_counts[row,r] <- p_value
 }
 
-write.table(genome_counts, file="Genome_Counts_Fishers_values", quote=F ,sep="\t",row.names=F,col.names=F)
+write.table(progeny_counts, file="Genome_Counts_Fishers_values", quote=F ,sep="\t",row.names=F,col.names=F)
