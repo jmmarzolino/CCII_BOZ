cb47a130228da00a7835e306b1ad6d24942b4b01
Jill Marzolino <jmarz001@ucr.edu>
2020-01-21 16:34:45 -0800

    rm unnamed df col specification (unnecessary) & a stray row reference

--- Fst.R
+++ Fst.R
@@ -12,7 +12,7 @@ library(readr)
 options(stringsAsFactors = F)
 
 df <- read_delim("minor_frequencies","\t", col_names = FALSE, trim_ws = TRUE)
-outdf[,1:2] <- df[,1:2]
+outdf <- df[,1:2]
 p_parent <- df[,3]
 
 for (i in 4:8){
@@ -20,7 +20,7 @@ for (i in 4:8){
   p_bar <- ((p_parent + p_progeny)/2)
   p_var <- (((p_progeny - p_bar) * (p_progeny - p_bar))/2)
   Fst <- ((p_var)/(p_bar))
-  outdf[row,(i-1)] <- Fst
+  outdf[,(i-1)] <- Fst
   }
 
 colnames(outdf) <- c("chr", "pos", "F18-to-parent", "F27-to-parent", "F28-to-parent", "F50-to-parent", "F58-to-parent")
